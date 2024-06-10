e31df1d600ad443a41e7b9e7444c1b60f9c378eb
AsGhrK <davicasari1@gmail.com>
2024-06-10 17:45:57 -0400

    10-06-24
    rotas para atulaizar e cadastro e mostra os usauarios

--- index.js
+++ index.js
@@ -6,12 +6,13 @@ const Jogo = require("./models/Jogo");
 const handlebars = require("express-handlebars");
 const { json } = require("express/lib/response");
 const Usuario = require("./models/Usuario");
+const res = require("express/lib/response");
 
 const app = express();
 
 // HandleBars
 app.engine("handlebars", handlebars.engine())
-app.set ("view engine", "handlebars")
+app.set("view engine", "handlebars")
 
 // Configurações do middleware
 app.use(express.urlencoded({ extended: true }));
@@ -37,14 +38,14 @@ app.post("/usuario/novo", async (req, res) => {
   }
 
   const usuario = await Usuario.create(dadosUsuario);
-  res.send("Jogo cadastrado com id" + usuario.id);
+  res.send("Usuario cadastrado com id" + usuario.id);
 });
 
 app.get("/jogo/novo", (req, res) => {
   res.render(`formJogo`);
 });
 
-app.get("/usuario/novo", (req, res) => {
+app.get("/usuarios/novo", (req, res) => {
   res.render(`formUsuario`);
 });
 
@@ -53,13 +54,21 @@ app.get("/", (req, res) => {
   res.render(`home`);
 });
 
-app.get("/usuarios", (req, res) => {
-  res.render(`usuarios`);
-});
+app.get("/usuarios", async (req, res) => {
+  const usuarios = await Usuario.findAll({ raw: true });
+  res.render("usuarios", { usuarios })
+})
+
+app.get("/usuarios/:id/atualizar", async (req, res) => {
+  const id = req.params.id
+  const usuario = await Usuario.findByPk(id, { raw: true })
+  res.render("formUsuario", { usuario });
+})
 
 // Inicialização do servidor
 app.listen(8000, () => {
   console.log("Abridu!");
+  console.log("http://localhost:8000/")
 });
 
 // Conexão com o banco de dados
