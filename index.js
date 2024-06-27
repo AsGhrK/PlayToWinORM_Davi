af1bf4245d17706f000a7f9f0b7b8e3d918ef089
AsGhrK <davicasari1@gmail.com>
2024-06-27 17:29:38 -0400

    27-06-24
    adicionado o cadastro de cartão para cada usuário

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const Jogo = require("./models/Jogo");
 const handlebars = require("express-handlebars");
 const { json } = require("express/lib/response");
 const Usuario = require("./models/Usuario");
+const Cartao = require("./models/Cartao");
 const res = require("express/lib/response");
+const { DataTypes } = require("sequelize");
 
 const app = express();
 
@@ -27,7 +29,7 @@ app.post("/jogo/novo", async (req, res) => {
   };
 
   const jogo = await Jogo.create(dadosJogo);
-  res.send("Jogo cadastrado com id " + jogo.id + `<a href= "http://localhost:8000/usuarios"> Voltar </a>`);
+  res.send("Jogo cadastrado com id " + jogo.id + `<a href= "http://localhost:8000/jogos"> Voltar </a>`);
 });
 
 
@@ -41,7 +43,7 @@ app.post("/usuario/novo", async (req, res) => {
   res.send("Usuário cadastrado com id " + usuario.id + `<a href= "http://localhost:8000/usuarios"> Voltar </a>`);
 });
 
-app.get("/jogo/novo", (req, res) => {
+app.get("/jogos/novo", (req, res) => {
   res.render("formJogo");
 });
 
@@ -53,6 +55,46 @@ app.get("/usuarios/novo", (req, res) => {
 app.get("/", (req, res) => {
   res.render("home");
 });
+
+app.get("/jogos", async (req, res) => {
+  const jogos = await Jogo.findAll({ raw: true });
+  res.render("jogos", { jogos });
+});
+
+app.get("/jogos/:id/atualizar", async (req, res) => {
+  const id = req.params.id;
+  const jogo = await Jogo.findByPk(id, { raw: true });
+  res.render("formJogo", { jogo });
+});
+
+
+app.post("/jogos/:id/atualizar", async (req, res) => {
+  const id = req.params.id;
+
+  const dadosJogo = {
+    titulo: req.body.titulo,
+    descricao: req.body.descricao,
+    precoBase: req.body.precoBase
+  };
+  const registroAfetados = await Jogo.update(dadosJogo, { where: { id: id } });
+  if (registroAfetados > 0) {
+    res.redirect("/jogos");
+  } else {
+    res.send("Erro ao atualizar jogo");
+  }
+});
+
+app.post("/jogos/excluir", async (req, res) => {
+  const id = req.body.id
+  const registroAfetados = await Jogo.destroy({ where: { id: id } });
+
+  if (registroAfetados > 0) {
+    res.redirect("/jogos");
+  } else {
+    res.send("Erro ao excluir jogo");
+  }
+});
+
 
 app.get("/usuarios", async (req, res) => {
   const usuarios = await Usuario.findAll({ raw: true });
@@ -92,6 +134,44 @@ app.post("/usuarios/excluir", async (req, res) => {
   }
 });
 
+
+// rotas Cartoes
+
+app.get('/usuarios/:id/cartoes', async (req, res) => {
+  const id = parseInt(req.params.id)
+  const usuario = await Usuario.findByPk(id, { include: ["Cartaos"] });
+
+  let cartoes = usuario.Cartaos;
+  cartoes = cartoes.map((cartao) => cartao.toJSON())
+
+
+  res.render("cartoes.handlebars", { usuario: usuario.toJSON(), cartoes });
+});
+
+//Formulário de cadastro de cartão
+app.get("/usuarios/:id/novoCartao", async (req, res) => {
+  const id = parseInt(req.params.id);
+  const usuario = await Usuario.findByPk(id, { raw: true });
+
+  res.render("formCartao", { usuario });
+});
+
+
+app.post("/usuarios/:id/novoCartao", async (req, res) => {
+  const id = parseInt(req.params.id);
+
+  const dadosCartao = {
+    numero: req.body.numero,
+    nome: req.body.nome,
+    cvv: req.body.codSeguranca,
+    UsuarioId: id,
+  };
+
+  await Cartao.create(dadosCartao);
+
+  res.redirect(`/usuarios/${id}/cartoes`);
+});
+
 // Inicialização do servidor
 app.listen(8000, () => {
   console.log("Servidor rodando em http://localhost:8000/");
@@ -99,7 +179,7 @@ app.listen(8000, () => {
 
 // Conexão com o banco de dados
 conn
-  .sync()
+  .sync({force:true})
   .then(() => {
     console.log("Conectado ao banco de dados com sucesso!");
   })
